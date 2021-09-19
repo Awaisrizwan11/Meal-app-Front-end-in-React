1adf6616942c3225d81c208139f68381e01068fc
Awais <asrncy@gmail.com>
2021-09-19 15:33:32 +0500

    Meal app in react

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+
+    return <div style={{backgroundColor:"blue"}}>
+
+      
+            <p style={{color:"white"}}>Desgin By</p>
+            <p style={{color:"white"}}>Awais Rizwan</p>
+       
+    </div>
+    
+}
+export default Footer
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Navbar() {
+
+    return <div style={{backgroundColor:"brown"}}>
+
+        <h1 >Meal App</h1>
+
+        <a style={{color:"white",margin:"10px"}}>Home </a>
+        <a style={{color:"white",margin:"10px"}}>Order</a>
+        <a style={{color:"white",margin:"10px"}}>Contact Us</a>
+
+    </div>
+    
+}
+export default Navbar
